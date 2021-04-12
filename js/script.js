4dab466764fc2e4efe4a0d0b8b7a1b6f454c345b
vetiann <47779644+vetiann@users.noreply.github.com>
2021-04-12 09:24:46 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,19 +23,18 @@ function requestTestimonies() {
                 const card = `
                 <div class="review_card" id="review_card_${x.id}">
                     <div class="row">
-                        <div class="col-md-2 user_info">
+                        <div class="col-md-1 user_info">
                             <figure><img src="img/avatar4.jpg" alt=""></figure>
-                            <h5>${x.author.name}</h5>
                         </div>
                         <div class="col-md-10 review_content">
                             <div class="clearfix add_bottom_15"></div>
-                            <h4>${x.text}</h4>
+                            <h5>${x.author.name}, ${x.author.age} Tahun, ${x.author.address}</h5>
                             <p>${x.text}</p>
                             <ul>
                                 <li>
                                     <a href="#" id=like_button_t_${x.id} onclick="return testimonyLikeClickHandler(this, ${x.id})">
                                         <i class="icon_like_alt"></i>
-                                        <span>Suka</span>
+                                        <span>Suka ${x.like_count}</span>
                                     </a>
                                 </li>
                                 <br>
