e8fb7c2ce3dc8bd00840917a20fd114c2c509a5b
ayusrya <81627960+ayusrya@users.noreply.github.com>
2021-04-22 13:36:41 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,70 @@
+function submitTestimonyClickHandler() {
+    //baris dibawah menangkap nilai dari isian form
+    let form_fullname = $("#fullName").val()
+    let form_nickname = $("#nickName").val()
+    let form_gender = $("input[name='gender']:checked").val()
+    let form_dateOfBirth = $("#dateOfBirth").val()
+    let form_nik = $("#nik").val()
+    let form_domicile = $("#domicile").val()
+    let form_w3review = $("#w3review").val()
+
+    
+    $.ajax({
+        type: "POST",
+        url: `https://x-dtpl.ridhopratama.net/vaccine/testimony`,
+        data: JSON.stringify({
+            name: form_fullname,
+            dob: form_dateOfBirth,
+            adress: form_domicile,
+            text: form_w3review,
+            gender: form_gender,
+            nik: form_nik,
+        }),
+        success: function (response) {
+            $('#form').modal('hide');
+            alert("Data testimony berhasil disimpan")
+        },
+        error: function(jqxhr){
+            alert("Proses penyimpanan data gagal: " + jqxhr.responseJSON.error.message);
+        }
+    })
+
+    return false
+}
+
+function loginClickHandler() {
+    let form_username = $("#username").val()
+    let form_password = $("#password").val()
+    
+    if(form_username=='' || form_password==''){
+        alert("Isian username dan password harus lengkap")
+    }
+    else{
+        $.ajax({
+            type: "POST",
+            url: `https://x-dtpl.ridhopratama.net/auth/login`,
+            data: JSON.stringify({username:form_username,password: form_password}),
+            success: function (response) {
+                localStorage['token_key'] = response.data.token;
+                $('#form_login').modal('hide');
+                setAfterAuth()
+            },
+            error: function(jqxhr){
+                alert("Login Gagal: " + jqxhr.responseJSON.error.message);
+            }
+        })
+    }
+
+    return false
+}
+
+
+function logoutClickHandler() {
+    localStorage['token_key'] = "";
+    location.reload()
+}
+
+
 function testimonyLikeClickHandler(element, testimonyID) {
     $.ajax({
         type: "POST",
@@ -26,7 +93,7 @@ function requestTestimonies() {
                         <div class="col-md-1 user_info">
                             <div id="profile_image">${x.author.name.charAt(0)}</div>
                         </div>
-                        <div class="col-md-10 review_content">
+                        <div class="col-md-9 review_content">
                             <div class="clearfix add_bottom_15"></div>
                             <h5>${x.author.name}, ${x.author.age} Tahun, ${x.author.address}</h5>
                             <p>${x.text}</p>
@@ -53,4 +120,18 @@ function requestTestimonies() {
     });
 }
 
+function setAfterAuth(){
+    if(localStorage['token_key']==null || localStorage['token_key'].length==0){
+        $("#login").show();
+        $("#admin_url").hide();
+        $("#logout").hide();
+    }
+    else{
+        $("#login").hide();
+        $("#admin_url").show();
+        $("#logout").show();
+    }
+}
+
 requestTestimonies()
+setAfterAuth()
